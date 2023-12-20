8cbc3a92e2ecd16c7a947fa6def3fc4bbc925ddb
Zeynep Özdemir <zeynepozz2299@gmail.com>
2023-12-20 11:03:45 +0300

    initial commit

---
+++ amadeusTravelToFuture/src/main/java/com/zeynapp/amadeusTravelToFuture/AmadeusTravelToFutureApplication.java
@@ -0,0 +1,13 @@
+package com.zeynapp.amadeusTravelToFuture;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AmadeusTravelToFutureApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AmadeusTravelToFutureApplication.class, args);
+	}
+
+}
---
+++ amadeusTravelToFuture/src/test/java/com/zeynapp/amadeusTravelToFuture/AmadeusTravelToFutureApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zeynapp.amadeusTravelToFuture;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AmadeusTravelToFutureApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
