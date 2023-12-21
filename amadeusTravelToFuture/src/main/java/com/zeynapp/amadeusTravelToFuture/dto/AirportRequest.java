a93a36806a8f68f33f9cbfdbb634fc7a26f04323
Zeynep Özdemir <zeynepozz2299@gmail.com>
2023-12-21 11:34:55 +0300

    Removed unnecessary annotations

--- amadeusTravelToFuture/src/main/java/com/zeynapp/amadeusTravelToFuture/dto/AirportRequest.java
+++ amadeusTravelToFuture/src/main/java/com/zeynapp/amadeusTravelToFuture/dto/AirportRequest.java
@@ -3,9 +3,6 @@ package com.zeynapp.amadeusTravelToFuture.dto;
 import lombok.*;
 
 @Data
-@Builder
-@NoArgsConstructor
-@AllArgsConstructor
 public class AirportRequest {
     private String city;
 }
